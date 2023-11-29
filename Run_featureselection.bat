61b519fd3966456169eedf1469ed6ef878f3e32f
IoanPJ <35175670+IoanPJ@users.noreply.github.com>
2023-11-29 18:26:06 +0000

    commit_20231129

---
+++ Run_featureselection.bat
@@ -0,0 +1,4 @@
+python3 "Data Processing"\\automater_3.py 58
+python3 ML\\"Neural Network (For automation).py" 58
+python3 "Data Processing"\\automater_3.py 60
+python3 ML\\"Neural Network (For automation).py" 60
