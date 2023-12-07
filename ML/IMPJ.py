98547a618cddbc7b98bac2f400acb905188f1374
IoanPJ <35175670+IoanPJ@users.noreply.github.com>
2023-12-07 19:04:07 +0000

    commit_20231207

---
+++ ML/IMPJ.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+class DataProcessor:
+    def __init__(self):
+        pass
+
+    def choose_2_vars(self,classname,data,A,B):
+        '''
+        In 'data' all unknown BCUs are assigned 0. 
+        All BLLs are assigned 1 and all FSRQs are assigned 2.
+        Radio Galaxies = 3 and Other = 4
+
+        The function will mask all values except the 2 desired classes
+        '''
+
+        for x in data[classname].unique():
+            if x == A or x == B:
+                pass
+            else:
+                mask = data[classname] == x
+                data = data[~mask]
+            
+        return data.loc[:, data.columns != classname] , data[classname]
+    
+    def logtransform(self,data):
+        
+        featurelist = ['Pivot_Energy']
+
+        for x in featurelist:
+            data[x] = np.log(data[x])
+        return data
