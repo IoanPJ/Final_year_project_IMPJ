7b062e1bfbbe9ea4e9427e92064bd7b723daf66b
IoanPJ <35175670+IoanPJ@users.noreply.github.com>
2023-12-11 12:45:27 +0000

    commit_20231211

--- ML/IMPJ.py
+++ ML/IMPJ.py
@@ -13,15 +13,22 @@ class DataProcessor:
 
         The function will mask all values except the 2 desired classes
         '''
-
         for x in data[classname].unique():
             if x == A or x == B:
                 pass
             else:
                 mask = data[classname] == x
                 data = data[~mask]
-            
-        return data.loc[:, data.columns != classname] , data[classname]
+
+        Y = np.zeros(len(data[classname]))
+
+        for i in range(0,len(data[classname])):
+            if np.array(data[classname])[i] == A:
+                pass
+            if np.array(data[classname])[i] == B:
+                Y[i] = 1
+        print('check1')
+        return data.loc[:, data.columns != classname] , Y
     
     def logtransform(self,data):
         
